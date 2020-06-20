08724dd5260f6cb6f5670ecd567aecfcde16a827
svirom <svirom@yahoo.com>
2020-06-20 20:42:30 +0300

    cost optimization

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,14 @@ $(document).ready(function() {
     }
   });
 
+  // Video Button
+	$(document).on("click", ".button-wrapper .play-button", function() {
+		var $video = $(this).closest('.video-wrapper-inner').find('.iframe-wrapper iframe');
+		var src = $video.attr('src');
+		var buttonWrapper = $(this).closest('.video-wrapper-inner').find('.button-wrapper');
+
+		$video.attr('src', src + '&autoplay=1');
+		buttonWrapper.css("display", "none");
+	});
+
 })
